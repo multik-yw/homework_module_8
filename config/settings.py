0bd44ac715b10fdcd13dc19f4bd516ce620205bb
Alina Moskvina <alisevenmo@gmail.com>
2025-08-20 18:19:35 +0300

    homework 35.5 done

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -155,3 +156,11 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3",
+        }
+    }
